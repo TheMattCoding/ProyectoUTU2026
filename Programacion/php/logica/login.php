eed77c1664895ce2563e5465a7788166f10e9bde
SophiaaLara <brumlarasophia@gmail.com>
2026-08-28 18:04:19 -0300

    Página registro

--- Programacion/php/logica/login.php
+++ Programacion/php/logica/login.php
@@ -83,7 +83,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
 
         <div class="enlace-registro">
-            ¿No tienes una cuenta? <a href="#">Registrar</a>
+            ¿No tienes una cuenta? 
+            <a href="registro.php">Registrar</a>
         </div>
     </div>
 </body>
